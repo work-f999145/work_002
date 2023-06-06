2155d7f2834cb56f8b6949b8ddec04db560fe943
f999145 <f999145@yandex.ru>
2023-06-06 14:08:19 +0300

    update

--- executable/config.py
+++ executable/config.py
@@ -9,7 +9,7 @@ class initial_driver(NamedTuple):
     proxy: str
     start_url: str
     
-drive_1 = initial_driver(1280, 1300, -2560, 0, '45.81.76.252:8000', 'https://lenta.com/catalog/')
+drive_1 = initial_driver(1280, 1300, -2560, 0, '45.128.129.151:62910', 'https://lenta.com/catalog/')
 drive_2 = initial_driver(1280, 1300, -1280, 0, '45.139.110.3:8000', 'https://lenta.com/catalog/')
 drive_3 = initial_driver(1280, 1300, -2560, 0, '45.81.76.252:8000', 'https://lenta.com/catalog/')
 
