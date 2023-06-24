65a96939d0ab2008fe724a7e3c1d94cde39c4102
f999145 <f999145@yandex.ru>
2023-06-24 16:41:13 +0300

    update

--- get_pages_and_parser_one_worker.py
+++ get_pages_and_parser_one_worker.py
@@ -32,7 +32,7 @@ def get_pages_main() -> list[str]:
     # Список для возвращения данных
     manager = Manager()
     results = manager.list()
-    p1 = Process(target=_worker_get_pages, args=(the_queue, drive_1, results, False))
+    p1 = Process(target=_worker_get_pages, args=(the_queue, drive_1, results, True))
     # p2 = Process(target=_worker_get_pages, args=(the_queue, drive_2, results))
     # Запуск процессов
     p1.start()
